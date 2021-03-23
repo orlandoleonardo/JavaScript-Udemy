cf3717dae42e0d29de6e5d0cd65ae6c7186d9ffe
Leonardo <lorlando@est.frba.utn.edu.ar>
2021-03-23 00:47:49 -0300

    dias de la semana

--- 01-Fundamentos/assets/js/if-else.js
+++ 01-Fundamentos/assets/js/if-else.js
@@ -20,3 +20,24 @@ if (dia === 0){ //pregunta por valor y tipo (es identidad)
 } else if (dia === 1) {
     console.log('Lunes');
 }
+
+//Sin usar If Else, o switch, únicamente objetos
+
+dia = 3;
+//día de la semana
+
+//con objetos
+const diasLetras = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miércoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado'
+}
+
+//con un arreglo
+let diasLetras2 = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+
+console.log(diasLetras[dia]);
