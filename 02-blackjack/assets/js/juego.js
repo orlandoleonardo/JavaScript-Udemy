781f685a4b14007515d72ee2a4bf326cab172aa2
Leonardo <lorlando@est.frba.utn.edu.ar>
2021-04-02 19:33:47 -0300

    pedirCarta y valorCarta - video 51

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -28,3 +28,22 @@ const crearDeck = () => {
 }
 
 crearDeck();
+
+const pedirCarta = () => {
+
+    if(deck.length===0){
+        throw 'No hay más cartass en el deck';
+    }
+
+    return deck.pop();
+}
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1); //no alcanza con tomar solo la primera posición porque sino tomaría mal el 10
+    return (isNaN(valor)) ? //pregunta si no es un numero
+        ( valor === 'A') ? 11 : 10
+        : valor * 1; //al multiplicarlo por 1 el string se transforma en un numerico
+}
+
+//ejemplo de uso de las dos funciones
+// valorCarta(pedirCarta());
